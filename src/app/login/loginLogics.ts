5f8c80f75f75f2059fea58237d1d91b7f28a21de
SHINJAEHEE-DEV <asjh011969@gmail.com>
2026-03-25 14:42:48 +0900

    Feat : sso 로직 구현및 페이지 디자인 수정

--- src/app/login/loginLogics.ts
+++ src/app/login/loginLogics.ts
@@ -3,18 +3,11 @@ import { createClient } from "@/utils/supabase/client";
 // 브라우저 화면에서 동작하는 함수들이므로, 클라이언트용 Supabase를 가져옵니다.
 const supabase = createClient();
 
-export const handleKakaoLogin = () => {
-    
-}
 
-export const handleNaverLogin = () => {
-    
-}
-
-export const handleGoogleLogin = async () => {
+export const handleSocialLogin = async (provider: 'google' | 'kakao') => {
     // 구글 소셜 로그인 창을 띄우는 함수입니다.
     const { data, error } = await supabase.auth.signInWithOAuth({
-  provider: 'google',
+  provider: provider,
   options: {
     // 로그인을 무사히 마치면, 구글이 우리 사이트의 어느 주소로 사용자를 돌려보낼지 정합니다.
     // 방금 우리가 만든 /auth/callback 경로로 보내서 일회성 코드를 로그인 세션(쿠키)으로 교환하게 만듭니다.
@@ -25,5 +18,13 @@ export const handleGoogleLogin = async () => {
     },
   },
 })
+
+if (error) {
+    console.error('로그인 에러:', error)
+    alert('로그인 중 오류가 발생했습니다.')
+    return
+  }else{
+    console.log('로그인 성공:', data)
+  }
 
 }
